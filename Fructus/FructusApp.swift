93013cf7169a76c5de26a6996d68c818fb8506c4
lambui <lambui@lecle.co.kr>
2023-03-16 01:04:54 +0700

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by bui khac lam on 16/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
